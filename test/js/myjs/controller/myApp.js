6f13e6cd77d3163748f311c9e22f6d591a8546f6
Beck.Zhang <beck.zhang@softtek.com>
2016-12-19 22:45:21 +0800

    router operate success

--- test/js/myjs/controller/myApp.js
+++ test/js/myjs/controller/myApp.js
@@ -2,35 +2,83 @@
  * Created by beck.zhang on 12/16/2016.
  */
 
-var app = angular.module('myApp',['ui.router']);
+var app = angular.module('myApp', ['ui.router']);
 app.config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
 
   $stateProvider
-    .state('home',{
-      url:"/home",
-      templateUrl: "./home.html",
-      controller: "HomeController",
-      controllerAs: "ctrHome"
-    })
-    .state('login',{
-      url:'/pages',
-      templateUrl:'./pageNav.html',
-      controller:function ($scope) {
-        // $scope.$parent.ctrHome.isshow = true;
-        console.log("login success");
-      }
-    })
-
+      .state('home', {
+        url: "/home",
+        templateUrl: "./home.html",
+        controller:function ($scope) {
+          console.log("enter home");
+        }
+        // controller: "HomeController",
+        // controllerAs: "ctrHome"
+      })
+      .state('login', {
+        url: '/pages',
+        views: {
+          "": {
+            templateUrl: './pageNav.html',
+            controller: function ($scope) {
+              //$scope.$parent.ctrHome.isshow = true;
+              console.log("login success");
+            }
+          },
+          "pages": {
+            templateUrl: "./page1.html",
+            controller: function ($scope) {
+              console.log("first enter page1");
+            }
+          }
+        }
+      })
+      .state('page1', {
+        url: '/page1',
+        views: {
+          "": {
+            templateUrl: './pageNav.html',
+            controller: function ($scope) {
+              //$scope.$parent.ctrHome.isshow = true;
+              console.log("login success");
+            }
+          },
+          "pages": {
+            templateUrl: "./page1.html",
+            controller: function ($scope) {
+              console.log("enter page1")
+            }
+          }
+        }
+      })
+      .state('page2', {
+        url: '/page2',
+        views: {
+          "": {
+            templateUrl: './pageNav.html',
+            controller: function ($scope) {
+              //$scope.$parent.ctrHome.isshow = true;
+              console.log("login success");
+            }
+          },
+          pages: {
+            templateUrl: './page2.html',
+            controller: function ($scope) {
+              console.log("enter page2")
+            }
+          }
+        }
+      })
 
 });
 
 app.controller('myCtrl', function myCtrl($scope, $http, $state, $stateParams) {
   $scope.users = [
-    {name:"Lemon Yang",admin:"Beck",date:"2016-12-13",lastDate:"2016-12-19"},
-    {name:"Lemon Yang",admin:"Beck",date:"2016-12-13",lastDate:"2016-12-19"},
-    {name:"Lemon Yang",admin:"Beck",date:"2016-12-13",lastDate:"2016-12-19"},
-    {name:"Lemon Yang",admin:"Beck",date:"2016-12-13",lastDate:"2016-12-19"}
+    {name: "Lemon Yang", admin: "Beck", date: "2016-12-13", lastDate: "2016-12-19"},
+    {name: "Lemon Yang", admin: "Beck", date: "2016-12-13", lastDate: "2016-12-19"},
+    {name: "Lemon Yang", admin: "Beck", date: "2016-12-13", lastDate: "2016-12-19"},
+    {name: "Lemon Yang", admin: "Beck", date: "2016-12-13", lastDate: "2016-12-19"}
   ];
 
 });
