c446678f93902ccc8337084ba22bbe08c1cba8f4
banana <banana.cao@softtek.com>
2016-12-23 13:30:57 +0800

    remove the newTest

--- newTest/database/usersql.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by banana.cao on 12/21/2016.
- */
-var UserSQL = {
-    insert:'INSERT INTO tb_users(name,createname,date,updates) VALUES(?,?,?,?)',
-    queryAll:'SELECT * FROM tb_users',
-    getUserById:'SELECT * FROM tb_users WHERE uid = ? ',
-};
-module.exports = UserSQL;
