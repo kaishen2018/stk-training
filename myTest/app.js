f36f2da8f54f81e338a6c9a2c17d8c93cc970803
banana <banana.cao@softtek.com>
2016-12-26 09:02:43 +0800

    I update the file

--- myTest/app.js
+++ myTest/app.js
@@ -10,8 +10,11 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setup（设定views变量，意为视图存放的目录）
+////express默认初始化模板路径,以后模板中包含的所有路径都是相对views这个文件下的,
+// 所以最好把多个路径的模板统一放到views文件夹下,我觉得这样才规范。
 app.set('views', path.join(__dirname, 'views'));
+//设定view engine变量，意为网页模板引擎
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -20,6 +23,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// 设定静态文件目录，比如本地文件
+// 目录为demo/public/images，访问
+// 网址则显示为http://localhost:3000/images
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', myCtrl);
@@ -33,7 +39,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
